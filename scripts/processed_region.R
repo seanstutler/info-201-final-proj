037c34fd804767769bceb06e8ed6c851059b5819
velocityCavalry <xyu530@uw.edu>
2018-05-17 22:18:46 -0700

    Fixes #6

--- scripts/processed_region.R
+++ scripts/processed_region.R
@@ -8,11 +8,13 @@ country_data <-
   read.csv(file = "../origin_data/WDI_csv/WDICountry.csv",
            stringsAsFactors = FALSE)
 
+#processed and extract data
 country_data <- country_data %>%
   filter(Region == "South Asia" | Region == "Europe & Central Asia" |
            Region == "East Asia & Pacific") %>%
   select(Country.Code, Short.Name, Region, Income.Group) %>%
   arrange(Short.Name)
 
+#write a new csv file for future data processed
 write.csv(country_data, file = "../processed_data/country.csv",
           row.names = FALSE)
