813d94bb8a1eec8aa4f0281593ff4083ccf4ecc9
Yangyu Chen <cheny289@uw.edu>
2018-05-24 20:59:53 -0700

    modified

--- scripts/data_process.R
+++ scripts/data_process.R
@@ -1,15 +1,29 @@
 library(dplyr)
-country_names <- read.csv(file = "../processed_data/country.csv", stringsAsFactors = FALSE)
+country_names <- read.csv(file = "../processed_data/region.csv", stringsAsFactors = FALSE)
+income_group <- read.csv(file = "../processed_data/income_group.csv", stringsAsFactors = FALSE)
 data <- read.csv(file = "../origin_data/WDIData.csv", stringsAsFactors = FALSE)
 country_names_list <- country_names$Country.Code
-processed <- data %>%
-    filter(Indicator.Name == "GDP growth (annual %)" | Indicator.Name == "GDP per capita (current US$)" |
-               Indicator.Name == "Adjusted savings: education expenditure (current US$)" |
-               Indicator.Name == "Birth rate, crude (per 1,000 people)" |
-               Indicator.Name == "Death rate, crude (per 1,000 people)" |
-               Indicator.Name == "Coverage of social insurance programs (% of population)" |
-               Indicator.Name == "Employment to population ratio, 15+, total (%) (national estimate)" |
-               Indicator.Name == "Access to electricity (% of population)" |
-               Indicator.Name == "Forest area (% of land area)") %>%
-    filter(Country.Code %in% country_names_list)
-write.csv(processed, file = "../processed_data/filted_general_data.csv", row.names = FALSE)
+income_list <- income_group$Country.Code
+income_type <- income_group %>%
+  select(income_group)
+
+
+process <- function(data, list, name) {
+  processed <- data %>%
+    filter(Indicator.Name == "Employment to population ratio, 15+, total (%) (national estimate)" |
+                 Indicator.Name == "GDP per capita (current US$)" |
+                 Indicator.Name == "Exports of goods and services (current US$)" |
+                 Indicator.Name == "Imports of goods and services (current US$)" |
+                 Indicator.Name == "Life expectancy at birth, total (years)" |
+                 Indicator.Name == "Literacy rate, adult total (% of people ages 15 and above)") %>%
+    filter(Country.Code %in% list)
+
+
+
+  processed[is.na(processed)] <- "Not Available"
+  write.csv(processed, file = paste0("../processed_data/", name, ".csv"), row.names = FALSE)
+}
+
+process(data, country_names_list, "country_indicators")
+
+process(data, income_list, "income_indicators")
