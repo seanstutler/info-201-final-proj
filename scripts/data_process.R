92fd112355cd429e971cabf0e75750431a6b4949
Yangyu Chen <cheny289@uw.edu>
2018-05-27 17:16:15 -0700

    make map

--- scripts/data_process.R
+++ scripts/data_process.R
@@ -19,11 +19,10 @@ process <- function(data, list) {
                  Indicator.Name == "Imports of goods and services (current US$)" |
                  Indicator.Name == "Life expectancy at birth, total (years)" |
                  Indicator.Name == "Literacy rate, adult total (% of people ages 15 and above)" |
-                 Indicator.Name == "GDP (current US$)")
-  %>%
+                 Indicator.Name == "GDP (current US$)") %>%
     filter(Country.Code %in% list)
 
-  processed[is.na(processed)] <- "Not Available"
+  processed[is.na(processed)] <- 0
   return(processed)
 }
 
--- scripts/map.R
+++ scripts/map.R
@@ -1,22 +1,31 @@
 library(plotly)
-data <- read.csv(file = "../processed_data/country_indicators.csv", stringsAsFactors = FALSE)
+country_data <- read.csv(file = "../processed_data/country_indicators.csv", stringsAsFactors = FALSE)
 # credit to https://plot.ly/r/choropleth-maps/
+
+choose_region <- function(data, region) {
 l <- list(color = toRGB("grey"), width = 0.5)
 
-# specify map projection/options
 g <- list(
+  scope = region,
   showframe = FALSE,
   showcoastlines = FALSE,
   projection = list(type = "natural earth")
 )
 
-p <- plot_geo(df) %>%
+gdp <- data[data$Indicator.Name == "GDP (current US$)", ]
+gdp$X2016 <- gdp$X2016 / 1000000000
+
+
+p <- plot_geo(gdp) %>%
   add_trace(
-    z = ~GDP..BILLIONS., color = ~GDP..BILLIONS., colors = 'Blues',
-    text = ~COUNTRY, locations = ~Country.Code, marker = list(line = l)
+    z = ~X2016, color = ~X2016, colors = 'Greens',
+    text = ~Country.Name, locations = ~Country.Code, marker = list(line = l)
   ) %>%
-  colorbar(title = 'GDP Billions US$', tickprefix = '$') %>%
+  colorbar(title = 'GDP Billions US$ (2016)', tickprefix = '$') %>%
   layout(
-    title = '2014 Global GDP<br>Source:<a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html">CIA World Factbook</a>',
+    title = "World Map",
     geo = g
   )
+return(p)
+
+}
