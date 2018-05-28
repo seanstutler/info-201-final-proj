12f59118cc9b0f675d99761014fc3d646ac1cfd6
YANG JUNXI <yangjunxi@D-10-19-183-21.dhcp4.washington.edu>
2018-05-27 17:40:03 -0700

    Update map

--- scripts/server.R
+++ scripts/server.R
@@ -3,11 +3,16 @@ library(shiny)
 
 # Read in data
 source('return_graph_from_param.R')
+source('map.R')
 df <- read.csv('../processed_data/country_indicators.csv',
                stringsAsFactors = FALSE)
 
 # Start the shiny server
 shinyServer(function(input, output) {
+
+  output$map <- renderPlotly({
+    return(choose_region(df, input$region))
+  })
 
   # Render a plotly scatter object
   output$scatter <- renderPlotly({
