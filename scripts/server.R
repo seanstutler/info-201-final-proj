f5805f5a7d9cec7a61b2a6b434823bb77de816b6
velocityCavalry <xyu530@uw.edu>
2018-05-28 21:15:12 -0700

    render ui

--- scripts/server.R
+++ scripts/server.R
@@ -24,10 +24,10 @@ shinyServer(function(input, output) {
     y <- df[[input$indicator]]
   })
 
-  output$countryAvailable <- reactive({
-    mdf <- df %<%
-      filter(Region == input$region)
-    return()
+output$countryAvailable <- reactive({
+  mdf <- df %<%
+  filter(Region == input$region)
+   return(mdf)
   })
 
 })
